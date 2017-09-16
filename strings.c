e22758549967c3c60749110167adbfb0f7edde68
Ben Puderer <bpuderer@yahoo.com>
2017-09-15 22:01:50 -0400

    strings

--- strings.c
+++ strings.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
+int string_length(char*);
+void reverse(char*);
+
 int main(void) {
 
     /* no string type in C */
@@ -8,7 +11,7 @@ int main(void) {
     char name[5];
     char name2[] = "Michael";
     char name3[10];
-    char name4[] = "John ";
+    char name4[20] = "John ";
     char name5[] = "Graham";
     char name6[] = "Terry";
 
@@ -19,9 +22,34 @@ int main(void) {
     name[4] = '\0';
 
     strcpy(name3, "Terry");
+    strcat(name4, name5);
 
     printf("\"%s\" has length %zd\n", name3, strlen(name3));
-    printf("%s %s %s %s %s\n", name, name2, name3, strcat(name4, name5), name6);
+    printf("%s %s %s %s %s\n", name, name2, name3, name4, name6);
     printf("strcmp(\"%s\", \"%s\") : %d\n", name3, name6, strcmp(name3, name6));
     printf("strcmp(\"%s\", \"%s\") : %d\n", name, name3, strcmp(name, name3));
+
+    printf("\"%s\" has length %d\n", name6, string_length(name6));
+
+    printf("%s reversed : ", name);
+    reverse(name);
+    printf("%s\n", name);
+}
+
+int string_length(char *str) {
+    /* useless, obviously */
+    int count;
+    for (count=0; str[count] != '\0'; ++count);
+    return count;
+}
+
+void reverse(char *str) {
+    if (str) {
+        char *end = str + strlen(str) - 1;
+        while (str < end) {
+            char tmp = *str;
+            *str++ = *end;
+            *end-- = tmp;
+        }
+    }
 }
